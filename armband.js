92e08c5e58ae15e630cdc482db94946ce8c3d3a9
Timotius Sitorus <timsitorus1@gmail.com>
2015-05-24 19:05:43 -0400

    migrate to official myojs

--- armband.js
+++ armband.js
@@ -1,9 +1,9 @@
 var arDrone = require('ar-drone');
-var Myo = require('myojs');
+var Myo = require('myo');
 var keypress = require('keypress');
 
 var client = arDrone.createClient();
-var hub = new Myo.Hub();
+var myMyo = Myo.create();
 
 keypress(process.stdin);
 process.stdin.setRawMode(true);
@@ -27,87 +27,69 @@ process.stdin.on('keypress', function(ch, key) {
 });
 
 var take_command = false;
-var newAngles;
-var zeroPos = null;
 var curAngles;
 // speeds can be anything between 0 and 1
 var yawSpeed = 0.5;
 var pitchSpeed = 0.5;
 var rollSpeed = 0.5;
 
-/*
-Pose types:
-0 - Rest
-1 - Fist
-2 - Wave In
-3 - Wave Out
-4 - Fingers Spread
-5 - Double Tap
-*/
 
-var getAnglesRelativeToStart = function(input) {
-    // obtains the current angular position of arm relative to the starting zero position 
-    newAngles = {
-        roll: Math.round(Math.abs(input.roll - zeroPos.roll) * 10),
-        pitch: Math.round(Math.abs(input.pitch - zeroPos.pitch) * 10),
-        yaw: Math.round(Math.abs(input.yaw - zeroPos.yaw) * 10)
-    };
-    return newAngles;
-};
 
-var getAngles = function(frame) {
+var getAngles = function(orientation) {
     // gives us the roll, pitch, and yaw
     var values = {
-        roll: Math.atan2(2.0 * (frame.rotation.w * frame.rotation.x + frame.rotation.y * frame.rotation.z), 1.0 - 2.0 * (frame.rotation.x * frame.rotation.x + frame.rotation.y * frame.rotation.y)),
-        pitch: Math.asin(Math.max(-1.0, Math.min(1.0, 2.0 * (frame.rotation.w * frame.rotation.y - frame.rotation.z * frame.rotation.x)))),
-        yaw: Math.atan2(2.0 * (frame.rotation.w * frame.rotation.z + frame.rotation.x * frame.rotation.y), 1.0 - 2.0 * (frame.rotation.y * frame.rotation.y + frame.rotation.z * frame.rotation.z))
+        roll: Math.atan2(2.0 * (orientation.w * orientation.x + orientation.y * orientation.z), 1.0 - 2.0 * (orientation.x * orientation.x + orientation.y * orientation.y)),
+        pitch: Math.asin(Math.max(-1.0, Math.min(1.0, 2.0 * (orientation.w * orientation.y - orientation.z * orientation.x)))),
+        yaw: Math.atan2(2.0 * (orientation.w * orientation.z + orientation.x * orientation.y), 1.0 - 2.0 * (orientation.y * orientation.y + orientation.z * orientation.z))
     };
     return values;
 
 };
-hub.on('connect', function() {
-    console.log("connected");
-});
+
 console.log("begin");
 
-hub.on('disconnect', function() {
+myMyo.on('connect', function() {
+    console.log("connected");
+});
+
+
+myMyo.on('disconnect', function() {
     console.log("disconnect");
 });
 
-hub.on('pose', function(pose) {
-    // takeoff
-    if (pose.type == 1) {
-        client.disableEmergency(); // in case drone crashed on previous run
-        take_command = true;
-        console.log("taking flight");
-        client.takeoff();
+myMyo.on('pose', function(pose_name, edge) {
+    if (edge) {
+        if (pose_name === 'fist') {
+            client.disableEmergency(); // in case drone crashed on previous run
+            client.takeoff();
+            take_command = true;
+            myMyo.zeroOrientation();
+            console.log("taking flight");
+        } 
+
+        else if (pose_name === 'thumb_to_pinky') {
+            take_command = false;
+            client.land();
+            console.log("landing");
+        } 
+
+        else if(pose_name === 'fingers_spread') {
+            myMyo.orientation();
+            console.log("position reset");
+        }
     }
-    // landing
-    else if (pose.type == 5) {
-        take_command = false;
-        console.log("landing");
-        client.land();
-    }
-    // reset the zero position to current position of arm
-    else if (pose.type == 4) {
-        zeroPos = curAngles;
-        console.log("position reset");
-    }
+
 });
 
-hub.on('frame', function(frame) {
+myMyo.on('orientation', function(frame) {
     if (take_command === true) {
         curAngles = getAngles(frame);
-        if (!zeroPos) {
-            zeroPos = curAngles;
-            console.log("reset zero position");
-        }
-        getAnglesRelativeToStart(curAngles);
-        var pitchPercent = newAngles.pitch / 10;
-        var yawPercent = newAngles.yaw / 10;
+
+        var pitchPercent = Math.abs(curAngles.pitch) / 10;
+        var yawPercent = Math.abs(curAngles.yaw) / 10;
         //var rollPercent = newAngles.yaw / ; // roll movements are less sensitive than others
 
-        if (newAngles.pitch > 3 && pitchPercent > yawPercent) {
+        if (Math.abs(curAngles.pitch) > 2 && pitchPercent > yawPercent) {
             if (curAngles.pitch > 0) {
                 console.log("down");
                 client.down(pitchSpeed);
@@ -115,7 +97,7 @@ hub.on('frame', function(frame) {
                 console.log("up");
                 client.up(pitchSpeed);
             }
-        } else if (newAngles.yaw > 2 && yawPercent > pitchPercent) {
+        } else if (Math.abs(curAngles.yaw) > 2 && yawPercent > pitchPercent) {
             if (curAngles.yaw - zeroPos.yaw > 0) {
                 console.log("back");
                 client.back(yawSpeed);
@@ -123,8 +105,8 @@ hub.on('frame', function(frame) {
                 console.log("front");
                 client.front(yawSpeed);
             }
-        } else if (newAngles.roll > 1) {
-            if (curAngles.roll - zeroPos.roll > 0) {
+        } else if (Math.abs(curAngles.roll) > 1) {
+            if (curAngles.roll  > 0) {
                 console.log("right");
                 client.right(rollSpeed);
             } else {
